6c7a4396a42fdcc2f99fc1d25f6c49b7566e2907
Mohammad Arasteh <arasteh@gmail.com>
2023-05-04 14:52:20 +0330

    feat: setup supabase

---
+++ src/types/supabase.ts
@@ -0,0 +1,110 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json }
+  | Json[]
+
+export interface Database {
+  public: {
+    Tables: {
+      comments: {
+        Row: {
+          comment_id: number
+          created_at: string | null
+          created_by: string | null
+          text: string | null
+        }
+        Insert: {
+          comment_id?: number
+          created_at?: string | null
+          created_by?: string | null
+          text?: string | null
+        }
+        Update: {
+          comment_id?: number
+          created_at?: string | null
+          created_by?: string | null
+          text?: string | null
+        }
+      }
+      posts: {
+        Row: {
+          body: string | null
+          comment_ids: string[] | null
+          created_at: string | null
+          created_by: string
+          edited_at: string | null
+          liked_by: string[] | null
+          post_id: number
+          tags: string[] | null
+          title: string
+          topic: string
+        }
+        Insert: {
+          body?: string | null
+          comment_ids?: string[] | null
+          created_at?: string | null
+          created_by: string
+          edited_at?: string | null
+          liked_by?: string[] | null
+          post_id?: number
+          tags?: string[] | null
+          title: string
+          topic: string
+        }
+        Update: {
+          body?: string | null
+          comment_ids?: string[] | null
+          created_at?: string | null
+          created_by?: string
+          edited_at?: string | null
+          liked_by?: string[] | null
+          post_id?: number
+          tags?: string[] | null
+          title?: string
+          topic?: string
+        }
+      }
+      users: {
+        Row: {
+          first_name: string | null
+          last_name: string | null
+          password: string
+          profile_url: string | null
+          user_id: number
+          user_name: string | null
+        }
+        Insert: {
+          first_name?: string | null
+          last_name?: string | null
+          password: string
+          profile_url?: string | null
+          user_id?: number
+          user_name?: string | null
+        }
+        Update: {
+          first_name?: string | null
+          last_name?: string | null
+          password?: string
+          profile_url?: string | null
+          user_id?: number
+          user_name?: string | null
+        }
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      [_ in never]: never
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+}
