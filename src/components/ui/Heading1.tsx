932e8b53388c4d5667a8edb259be100f6cc7f4e5
Mohammad Arasteh <arasteh@gmail.com>
2023-05-04 22:50:14 +0330

    feat(ui): implement simple h1 component

---
+++ src/components/ui/Heading1.tsx
@@ -0,0 +1,36 @@
+import { cva, VariantProps } from "cva";
+
+const heading1Variants = cva(
+  " text-center lg:text-left font-extrabold leading-tight tracking-tighter",
+  {
+    variants: {
+      size: {
+        default: "text-4xl md:text-5xl lg:text-6xl",
+        lg: "text-5xl md:text-6xl lg:text-7xl",
+        sm: "text-2xl md:text-3xl lg:text-4xl",
+      },
+    },
+    defaultVariants: {
+      size: "default",
+    },
+  }
+);
+
+interface Heading1Props
+  extends React.HTMLAttributes<HTMLHeadingElement>,
+    VariantProps<typeof heading1Variants> {}
+
+const Heading1: React.FC<Heading1Props> = ({
+  children,
+  className,
+  size,
+  ...props
+}) => {
+  return (
+    <h1 {...props} className={heading1Variants({ size, className })}>
+      {children}
+    </h1>
+  );
+};
+
+export default Heading1;
